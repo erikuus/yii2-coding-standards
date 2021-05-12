76f1d26c6e0deda1e18b35aa5541a39cfa357eba
Erik Uus <erik.uus@gmail.com>
2021-05-13 00:31:11 +0300

    Parandab küljendust

--- Tests/falsetestclass.php
+++ Tests/falsetestclass.php
@@ -2,54 +2,55 @@
 
 class trueTestClass
 {
-    const test_CONSTANT=true;
-    public $publicProperty=true;
-    private $privateProperty=true;
+	const test_CONSTANT=true;
+	public $publicProperty=true;
+	private $privateProperty=true;
 
-    public function publicMethod()
-    {
-        if(true)
-            return true;
-        else
-            return false;
+	public function publicMethod()
+	{
+		if(true)
+			return true;
+		else
+			return false;
 
-        while(true)
-            return true;
+		while(true)
+			return true;
 
-        for(true;true;true)
-            return true;
+		for(true;true;true)
+			return true;
 
-        foreach(array(true) as $var)
-            return true;
+		foreach(array(true) as $var)
+			return true;
 
-        if(true)
-        {
-            return true;
-        }
-        elseif(true)
-        {
-            return false;
-        }
-        else
-        {
-            return false;
-        }
+		if(true)
+		{
+			return true;
+		}
+		elseif(true)
+		{
+			return false;
+		}
+		else
+		{
+			return false;
+		}
 
-        while(true)
-        {
-            return true;
-        }
+		while(true)
+		{
+			return true;
+		}
 
-        for(true;true;true)
-        {
-            return true;
-        }
+		for(true;true;true)
+		{
+			return true;
+		}
 
-        foreach(array(true) as $var)
-        {
-            return true;
-        }
+		foreach(array(true) as $var)
+		{
+			return true;
+		}
 
 		return true;
-    }
+	}
 }
+
--- Yii2/PHPCSAliases.php
+++ Yii2/PHPCSAliases.php
@@ -23,31 +23,32 @@
  * external PHPCS standards creating cross-version compatibility in the same
  * manner.}}
  */
-if(! defined('WPCS_PHPCS_ALIASES_SET'))
-{
-	// PHPCS base classes/interface.
-	if(! interface_exists('\PHP_CodeSniffer_File'))
-		class_alias('PHP_CodeSniffer\Files\File', '\PHP_CodeSniffer_File');
+if (! defined('WPCS_PHPCS_ALIASES_SET')) {
+    // PHPCS base classes/interface.
+    if (! interface_exists('\PHP_CodeSniffer_File')) {
+        class_alias('PHP_CodeSniffer\Files\File', '\PHP_CodeSniffer_File');
+    }
 
-	if(! class_exists('\PHP_CodeSniffer_Tokens'))
-		class_alias('PHP_CodeSniffer\Util\Tokens', '\PHP_CodeSniffer_Tokens');
+    if (! class_exists('\PHP_CodeSniffer_Tokens')) {
+        class_alias('PHP_CodeSniffer\Util\Tokens', '\PHP_CodeSniffer_Tokens');
+    }
 
-	if(! class_exists('\PHP_CodeSniffer_Sniff'))
-		class_alias('PHP_CodeSniffer\Sniffs\Sniff', '\PHP_CodeSniffer_Sniff');
+    if (! class_exists('\PHP_CodeSniffer_Sniff')) {
+        class_alias('PHP_CodeSniffer\Sniffs\Sniff', '\PHP_CodeSniffer_Sniff');
+    }
 
-	// PHPCS classes which are being extended by WPCS sniffs.
-	if(! class_exists('\PHP_CodeSniffer_Standards_AbstractVariableSniff'))
-		class_alias('PHP_CodeSniffer\Sniffs\AbstractVariableSniff', '\PHP_CodeSniffer_Standards_AbstractVariableSniff');
+    // PHPCS classes which are being extended by WPCS sniffs.
+    if (! class_exists('\PHP_CodeSniffer_Standards_AbstractVariableSniff')) {
+        class_alias('PHP_CodeSniffer\Sniffs\AbstractVariableSniff', '\PHP_CodeSniffer_Standards_AbstractVariableSniff');
+    }
 
-	if(! class_exists('\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff'))
-	{
-		class_alias('PHP_CodeSniffer\Standards\PEAR\Sniffs\NamingConventions\ValidFunctionNameSniff', '\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff');
-	}
+    if (! class_exists('\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff')) {
+        class_alias('PHP_CodeSniffer\Standards\PEAR\Sniffs\NamingConventions\ValidFunctionNameSniff', '\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff');
+    }
 
-	if(! class_exists('\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff'))
-	{
-		class_alias('PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\OperatorSpacingSniff', '\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff');
-	}
+    if (! class_exists('\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff')) {
+        class_alias('PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\OperatorSpacingSniff', '\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff');
+    }
 
-	define('WPCS_PHPCS_ALIASES_SET', true);
+    define('WPCS_PHPCS_ALIASES_SET', true);
 }
